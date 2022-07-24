6ddcfa722719e9bf18f979e8b350f7ab8d3df755
JOYCE <joychinwenduibeh93@gmail.com>
2022-07-24 16:19:50 +0100

    my image gallery

---
+++ fitness training/main.js
@@ -0,0 +1,71 @@
+const change = document.getElementById('imageblue');
+change.addEventListener('click', (event) => {
+    event.target.style.background = 'blue';
+    event.target.style.color = 'white';
+});
+
+const change2 = document.getElementById('imagered');
+change2.addEventListener('click', (event) => {
+    event.target.style.background = 'red';
+    event.target.style.color = 'white';
+});
+
+const change3 = document.getElementById('imagepurple');
+change3.addEventListener('click', (event) => {
+    event.target.style.background = 'purple';
+    event.target.style.color = 'white';
+});
+
+const change4 = document.getElementById('imagegreen');
+change4.addEventListener('click', (event) => {
+    event.target.style.background = 'green';
+    event.target.style.color = 'white';
+});
+
+const change5 = document.getElementById('imageblue1');
+change5.addEventListener('click',(event) =>{
+    event.target.style.background = 'blue';
+    event.target.style.color = 'white';
+});
+
+const change6 = document.getElementById('imagered1');
+change6.addEventListener('click',(event) =>{
+    event.target.style.background = 'red';
+    event.target.style.color = 'white';
+});
+
+const change7 = document.getElementById('imagepurple1');
+change7.addEventListener('click',(event) =>{
+    event.target.style.background = 'purple';
+    event.target.style.color = 'white';
+});
+
+const change8 = document.getElementById('imagegreen1');
+change8.addEventListener('click',(event) =>{
+    event.target.style.background = 'green';
+    event.target.style.color = 'white';
+});
+
+const change9 = document.getElementById('imageblue2');
+change9.addEventListener('click',(event)=>{
+    event.target.style.background = 'blue';
+    event.target.style.color ='white'
+});
+
+const change10 = document.getElementById('imagered2');
+change10.addEventListener('click',(event)=>{
+    event.target.style.background = 'red';
+    event.target.style.color = 'white';
+});
+const change11 = document.getElementById('imagepurple2');
+change11.addEventListener('click',(event) =>{
+    event.target.style.background = 'purple';
+    event.target.style.color = 'white';
+});
+const change12 = document.getElementById('imagegreen2');
+change12.addEventListener('click',(event) =>{
+    event.target.style.background = 'green';
+    event.target.style.color = 'white';
+
+})
+
